e0aef4c7944902b7fc0c1e1b9a91aa7c9f223c6f
zhoufeng <zhoufeng@lvwan.com>
2019-01-25 10:47:44 +0800

    测试一下

--- app/src/main/java/j/com/kotlinmall/activty/MainActivity.kt
+++ app/src/main/java/j/com/kotlinmall/activty/MainActivity.kt
@@ -2,12 +2,14 @@ package j.com.kotlinmall.activty
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import j.com.kotlinmall.R
 
 class MainActivity : AppCompatActivity() {
-
+    val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.i(tag,"test")
     }
 }
